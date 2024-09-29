6f7802c3c7f982ecaabf9d47c4de19ae47e7d956
vufon <tieuquy.shop@gmail.com>
2024-09-30 00:59:23 +0700

    Test wasm module

--- public/background.js
+++ public/background.js
@@ -1,54 +1,15 @@
-chrome.alarms.onAlarm.addListener(function() {
-  let storedTime;
-  chrome.storage.local.get(['minutes'], function(result) {
-    storedTime = result.minutes
-    storedTime += 1;
-    chrome.storage.local.set({ minutes: storedTime });
-  })
-})
+import initWasmModule, { hello_background } from './wasm_mod.js';
 
-chrome.runtime.onMessage.addListener(function(message) {
-  if (message.id === 'timer') {
-    switch (message.payload) {
-      case 'timerOn':
-        chrome.storage.local.get(['minutes'], function(result) {
-          chrome.storage.local.set({
-            minutes: result.minutes ? result.minutes : 0
-          });
-        })
-        chrome.alarms.create({ delayInMinutes: 1, periodInMinutes: 1 });
-        chrome.action.setBadgeText({ text: 'ON' });
-        break;
-      case 'timerOff':
-        chrome.alarms.clearAll();
-        chrome.storage.local.set({ minutes: 0 });
-        chrome.action.setBadgeText({ text: '' });
-        break;
-      case 'timerPause':
-        chrome.alarms.clearAll();
-        chrome.action.setBadgeText({ text: 'HOLD' });
-        break;
-      case 'timerReset':
-        chrome.alarms.clearAll();
-        chrome.storage.local.set({ minutes: 0 });
-        chrome.alarms.create({ delayInMinutes: 1, periodInMinutes: 1 });
-        chrome.action.setBadgeText({ text: 'ON' });
-        break;
-      default: console.log('Message Error: No case found.')
-    }
-  } else if (message.id === 'tabs') {
-    chrome.storage.local.get(['urls'], function(result) {
-      const storedUrls = result.urls[message.groupId].urls
-      const isPinnedSelector = `${message.groupId}IsPinned`
-      const isPinned = result.urls[message.groupId][isPinnedSelector]
-      const urlsArray = Object.entries(storedUrls)
-      for (let item in urlsArray) {
-        chrome.tabs.create({ url: urlsArray[item][1], pinned: isPinned })
-      }
-    })
-  } else if (message.id === 'setUrls') {
-    chrome.storage.local.set({
-      urls: message.payload
+(async () => {
+  await initWasmModule();
+})();
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.id === 'wasm' && message.payload === 'print') {
+    //this function be called on wasm
+    hello_background()
+    sendResponse({
+      response: "response data"
     });
   } else {
     console.log("400 - Incorrect message.id")
